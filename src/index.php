7f6ce76b5889862701fb8a77a1addf18ec2ebf93
omarsaad08 <omarsaad08@outlook.com>
2025-11-30 10:16:36 +0200

    .

--- src/index.php
+++ src/index.php
@@ -1,10 +1,9 @@
 <?php
 // CORS Headers
 header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Origin, Accept");
+header("Access-Control-Allow-Headers: *");
+
 
 // Handle preflight OPTIONS request
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
